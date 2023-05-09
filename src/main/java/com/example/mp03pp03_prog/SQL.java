bca5b0a79f37a21a8c9775ff86c799a5f5f63a56
Orilinked <jakferresrivas@gmail.com>
2023-05-09 17:57:23 +0200

    Final_SQL

--- src/main/java/com/example/mp03pp03_prog/SQL.java
+++ src/main/java/com/example/mp03pp03_prog/SQL.java
@@ -1,8 +1,5 @@
 package com.example.mp03pp03_prog;
-import java.sql.Connection;
-import java.sql.Date;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class SQL {
     private String url;
@@ -14,7 +11,19 @@ public class SQL {
     private void guardar_dades(String nom, Float preu, String descripcio, Date data_caducitat){
         try {
             Connection conn = DriverManager.getConnection(url);
-            String sql = "INSERT INTO productes (nom, preu, descripcio,data_caducitat) VALUES ("+nom+","+preu+","+descripcio+","+data_caducitat+")";
+            String sql = "INSERT INTO productes (nom, preu, descripcio,data_caducitat) VALUES (?,?,?,?)";
+            // Crear un objeto PreparedStatement y establecer los valores de los parámetros
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, nom);
+            pstmt.setFloat(2, preu);
+            pstmt.setString(3,descripcio);
+            pstmt.setDate(4,data_caducitat);
+            // Ejecutar la sentencia SQL
+            int rowsAffected = pstmt.executeUpdate();
+            System.out.println(rowsAffected + " row(s) affected");
+            // Cerrar la conexión y el objeto PreparedStatement
+            pstmt.close();
+            conn.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -30,6 +39,28 @@ public class SQL {
                     ");";
         }catch (SQLException e){
             throw  new RuntimeException(e);
+        }
+    }
+
+    private void buscar_productes(String nom){
+        try {
+            Connection coon = DriverManager.getConnection(url);
+            String sql = "SELECT * FROM productes WHERE NOM =?;";
+            PreparedStatement pstmt = coon.prepareStatement(sql);
+            pstmt.setString(1, nom);
+            // Ejecutar la sentencia SQL
+            int rowsAffected = pstmt.executeUpdate();
+            System.out.println(rowsAffected + " row(s) affected");
+            // Cerrar la conexión y el objeto PreparedStatement
+            pstmt.close();
+            coon.close();
+            if (rowsAffected == 1){
+
+            }else {
+
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
